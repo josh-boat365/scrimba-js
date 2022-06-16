d31a4d41cfc5eabc97f83579e3446ffc00fe4289
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2022-06-16 14:46:42 -0700

    counter-app complete

--- counter-app/script.js
+++ counter-app/script.js
@@ -1,1 +1,43 @@
-document.getElementById('number-count').innerHTML = 3
+// Question 2
+// var myAge = 22
+// var humanDogRatio = 7
+// var myDogAge = myAge * humanDogRatio
+// console.log(myDogAge + " Dog Years Old")
+
+// Question 3
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+let people_count = document.getElementById('number-count')
+let counted_people = document.getElementById('counted-people')
+let count = 0
+
+function increment() {
+    count += 1
+    people_count.innerHTML = count
+
+    // return people_count;
+
+}
+
+function decrement() {
+    count -= 1
+    people_count.innerHTML = count
+
+    // return people_count;
+
+}
+
+function save() {
+    counted_people.innerHTML = count
+}
