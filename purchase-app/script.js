3caae9d0e80eab42d76b0c66408329425f8cc7b6
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2022-06-17 07:14:18 -0700

    purchase-app modified

---
+++ purchase-app/script.js
@@ -0,0 +1,7 @@
+let error_message = document.getElementById('error-message')
+let message = " "
+
+function error() {
+    message = "Something went wrong, try again!"
+    error_message.innerHTML = message
+}
